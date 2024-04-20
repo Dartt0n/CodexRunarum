ecbf5974c48283722e6c99f009de6e6b1d4ac867
Saveliy Khlebnov <savel.khlebnov@yandex.ru>
2024-04-21 01:08:08 +0300

    tree implementation

--- codexrunarum/core/elements/solid/base.py
+++ codexrunarum/core/elements/solid/base.py
@@ -8,3 +8,6 @@ from codexrunarum.core.elements.base import BaseElement
 class BaseSolid(BaseElement, ABC):
     def __init__(self, energy: float):
         super().__init__(energy, np.array((0, 0)))
+
+    def evolute(self, row: int, column: int) -> list[tuple[int, int, BaseElement]]:
+        return [(row, column, self)]
